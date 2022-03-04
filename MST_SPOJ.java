4bd1416fbae6edc89341da2a800105e6b0e2fb53
Zaid-R <81107281+Zaid-R@users.noreply.github.com>
2022-03-04 12:22:00 +0300

    Update MST_SPOJ.java

--- MST_SPOJ.java
+++ MST_SPOJ.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class MST_SPOJ {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
